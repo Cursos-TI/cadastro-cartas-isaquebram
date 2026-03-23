989f337d20a46e52fc960e84cd3166a2609373f7
isaquebram <isaque.bram@gmail.com>
2026-03-23 11:29:25 +0000

    codigo nivel novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -29,9 +29,9 @@ int main() {
     printf ("Estado : ");
     scanf(" %c" , &estado1);
     printf ("Codigo : ");
-    scanf("%s" , &codigo1);
+    scanf(" %s" , codigo1);
     printf ("Nome da cidade : ");
-    scanf(" %[^\n]" , &cidade1);
+    scanf(" %s" , cidade1);
     printf ("População : ");
     scanf("%d" , &populacao1);
     printf ("Area (km²) : ");
@@ -43,8 +43,42 @@ int main() {
 
   //>>>>> CADASTRO CARTA 2 <<<<<
     printf (">>> Cadastro Carta 2 <<< \n");
+    printf ("Estado : ");
+    scanf(" %c" , &estado2);
+    printf ("Codigo : ");
+    scanf(" %s" , codigo2);
+    printf ("Nome da cidade : ");
+    scanf(" %s" , cidade2);
+    printf ("População : ");
+    scanf("%d" , &populacao2);
+    printf ("Area (km²) : ");
+    scanf("%f" , &area2);
+    printf ("PIB : ");
+    scanf("%f" , &pib2);
+    printf ("Numero depontos turisticos : ");
+    scanf("%d" , &pontos2);
 
-  // Área para exibição dos dados da cidade
+  // Área para exibição dos dados da cidad
+  //  SAÍDA 
+  printf("\n\n>>>>> CARTAS CADASTRADAS <<<<\n");
+
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", cidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Pontos Turisticos: %d\n", pontos1);
+
+   printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", cidade2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Pontos Turisticos: %d\n", pontos2);
 
 return 0;
 } 
