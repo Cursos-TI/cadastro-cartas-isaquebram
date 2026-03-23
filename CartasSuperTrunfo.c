445aa87a18fd250e8c40a421d3ce19de9e8e91f5
isaquebram <isaque.bram@gmail.com>
2026-03-23 11:21:57 +0000

    codigo de nivel novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-// >>>>> CARTA 1 <<<<<
+  // >>>>> VAR. CARTA 1 <<<<<
     char estado1;
     char codigo1[4];
     char cidade1[40];
@@ -15,7 +15,7 @@ int main() {
     float pib1;
     int pontos1;
 
-    // >>>>> CARTA 2 <<<<<
+    // >>>>>VAR. CARTA 2 <<<<<
     char estado2;
     char codigo2[4];
     char cidade2[40];
@@ -24,6 +24,25 @@ int main() {
     float pib2;
     int pontos2;
   // Área para entrada de dados
+  //>>>>> CADASTRO CARTA 1 <<<<<
+    printf (">>> Cadastro Carta 1 <<< \n");
+    printf ("Estado : ");
+    scanf(" %c" , &estado1);
+    printf ("Codigo : ");
+    scanf("%s" , &codigo1);
+    printf ("Nome da cidade : ");
+    scanf(" %[^\n]" , &cidade1);
+    printf ("População : ");
+    scanf("%d" , &populacao1);
+    printf ("Area (km²) : ");
+    scanf("%f" , &area1);
+    printf ("PIB : ");
+    scanf("%f" , &pib1);
+    printf ("Numero depontos turisticos : ");
+    scanf("%d" , &pontos1);
+
+  //>>>>> CADASTRO CARTA 2 <<<<<
+    printf (">>> Cadastro Carta 2 <<< \n");
 
   // Área para exibição dos dados da cidade
 
