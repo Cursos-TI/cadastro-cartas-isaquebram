329304f33d10e25f97dc71a9c5df49d41ef8bcd4
isaquebram <isaque.bram@gmail.com>
2026-03-22 21:45:35 -0400

    Atualizar o CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,23 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+// >>>>> CARTA 1 <<<<<
+    char estado1;
+    char codigo1[4];
+    char cidade1[40];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontos1;
 
+    // >>>>> CARTA 2 <<<<<
+    char estado2;
+    char codigo2[4];
+    char cidade2[40];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos2;
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
